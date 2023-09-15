0c4fd2a5538e94ec86bf9b765c1cc41f9c121cef
Maciel Alves Pereira <catce.2023111mtds0003@aluno.ifpi.edu.br>
2023-09-15 16:58:59 -0300

    adicionado cnh.py e comentários de instruções

---
+++ cnh.py
@@ -0,0 +1,13 @@
+"""_summary_
+"""
+# inserir campo nome
+# inserir campo RG - orgão emissor - UF
+# inserir campo cpf - data/nascimento
+# inserir campo filiação -> campo para inserir o nome do pai e da mãe
+# inserir campo permissão - ACC - CAT.HAB -> (AD) -> [A, B, C, D ou E]
+# inserir campo Nº de Registro - Validade - 1ª Habilitação
+# inserir campo de Observações
+# inserir Campo destinado para <assinatura do portador> externa
+# inserir campo Local - data de emissão
+# inserir Campo destinado a <assinatura do emissor>
+# inserir em um lugar da folha a imagem de perfil
