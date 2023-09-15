ff63601431f30b8186273fdab317f49e14d7517f
Maciel Alves Pereira <catce.2023111mtds0003@aluno.ifpi.edu.br>
2023-09-15 17:17:21 -0300

    feat: inserido a estrutura da cnh

--- cnh.py
+++ cnh.py
@@ -1,5 +1,6 @@
 """_summary_
 """
+# charset_
 # inserir campo nome
 # inserir campo RG - orgão emissor - UF
 # inserir campo cpf - data/nascimento
@@ -11,3 +12,30 @@
 # inserir campo Local - data de emissão
 # inserir Campo destinado a <assinatura do emissor>
 # inserir em um lugar da folha a imagem de perfil
+
+
+class CNH:
+    nome = None
+    rg = None
+    cpf = None
+    filiacao = None  # (nome_mae, nome_pai)
+    permissao = None
+    acc = None
+    cats_hab = ('A', 'B', 'C', 'D', 'E')
+    cat_hab = None
+    n_reg = None
+    val = None
+    primery_hab = None
+    obs = None
+    data_emissao = None
+    img_profile = None
+
+    def __init__(self,
+                 nome, rg, cpf, filiacao, cat_hab, img_profile=None) -> None:
+        self.nome = nome
+        self.rg = rg
+        self.cpf = cpf
+        self.filiacao = filiacao
+        self.cat_hab = cat_hab
+        self.img_profile = img_profile
+        pass
